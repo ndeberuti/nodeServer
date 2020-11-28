fa01710e4bdf21fc337770c75731ca955a447039
ndeberuti <ndeberuti@gmail.com>
2020-11-28 16:31:38 -0300

    Add basic routing

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
-})
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW page');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT page');
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+        });
+        res.end('<h1> Page not found </h1>');
+    }
+});
 
 server.listen(8000, '127.0.0.1', ()=> {
     console.log('listening on port: 8000');
